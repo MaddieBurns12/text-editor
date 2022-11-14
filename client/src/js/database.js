233451b716ac5557726ec33c239521ecf4197685
Madeline Burns <mburns6023@gmail.com>
2022-11-13 21:23:07 -0500

    initial commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,11 +11,18 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+  
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  const req = req.objStore({id: id, value:value});
+  const res = await req;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const req = objStore.getAll();
+  const res = await req;
+};
 
 initdb();
